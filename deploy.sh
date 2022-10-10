6a03033e51e8f1666d42571b564ab1be9f4af468
wenhao <840346763@qq.com>
2022-10-10 22:05:05 +0800

    10.10

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,39 @@
 #!/usr/bin/env sh
-
 # 确保脚本抛出遇到的错误
 set -e
+npm run build # 生成静态文件
+cd docs/.vuepress/dist # 进入生成的文件夹
 
-# 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
-push_addr='https://github.com/whcoding/vuepress-blog.git' # git提交地址，也可以手动设置，比如：push_addr=git@github.com:xugaoyi/vuepress-theme-vdoing.git
-commit_info=`git describe --all --always --long`
-dist_path=docs/.vuepress/dist # 打包生成的文件夹路径
-push_branch=pages # 推送的分支
-
-# 生成静态文件
-npm run build
-
-# 进入生成的文件夹
-cd $dist_path
-
+# deploy to github
+# echo 'whcoding.com' > CNAME
+if [ -z "$GITHUB_TOKEN" ]; then
+  msg='deploy'
+  githubUrl=git@github.com:whcoding/vuepress-blog.git
+else
+  msg='来自github action的自动部署'
+  githubUrl=https://whcoding:${GITHUB_TOKEN}git@github.com:whcoding/vuepress-blog.git
+  git config --global user.name "whcoding"
+  git config --global user.email "840346763@qq.com"
+fi
 git init
 git add -A
-git commit -m "deploy, $commit_info"
-git push -f $push_addr HEAD:$push_branch
+git commit -m "${msg}"
+git push -f $githubUrl master:gh-pages # 推送到github
+
+# deploy to coding
+# echo 'www.xugaoyi.com\nxugaoyi.com' > CNAME  # 自定义域名
+# if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
+#   codingUrl=git@git.dev.tencent.com:xugaoyi/xugaoyi.git
+# else
+#   codingUrl=https://xugaoyi:${CODING_TOKEN}@git.dev.tencent.com/xugaoyi/xugaoyi.git
+# fi
+# git add -A
+# git commit -m "${msg}"
+# git push -f $codingUrl master # 推送到coding
+
+# deploy to tencent server
+
+
 
 cd -
-rm -rf $dist_path
+rm -rf docs/.vuepress/dist
--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -29,11 +29,8 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
     // 导航配置
     nav: [
       { text: '首页', link: '/' },
-      {
-        text: '收藏',
-        link: '/pages/d27dbc/'
-      },
-      {text: '归档',link: '/archives/'},
+      { text: '收藏', link: '/collectsss/' },
+      { text: '归档', link: '/archives/'},
       { text: '关于', link: '/about/' },
     ],
     sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
